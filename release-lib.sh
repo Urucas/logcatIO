cfa04c8c3505bf3654c3cb656b6cbbe9dcd8bbde
vrunoa <vruno.alassia@gmail.com>
2016-01-15 00:13:11 -0800

    update release script

--- release-lib.sh
+++ release-lib.sh
@@ -4,8 +4,13 @@ VERSION=$(./gradlew -q Version)
 echo $VERSION
 RELEASE_NAME=$"./logcatio-${VERSION}.aar"
 if [ ! -f $RELEASE_NAME ]; then
-  exit 1
   echo "buiild release $RELEASE_NAME not found!"
+  exit 1
+fi
+MAVEN_FILE="maven-metadata.xml"
+if [ ! -f $MAVEN_FILE ]; then
+  echo "buiild maven $MAVEN_FILE not found!"
+  exit 1
 fi
 git checkout gh-pages
 RELEASE_FOLDER=$"com/urucas/logcatio/${VERSION}/"
@@ -14,7 +19,10 @@ if [ ! -d $RELEASE_FOLDER ]; then
 fi
 RELEASE_PATH=$"${RELEASE_FOLDER}logcatio-${VERSION}.aar"
 mv  $RELEASE_NAME $RELEASE_PATH
+MAVEN_PATH=$"com/urucas/logcatio/$MAVEN_FILE"
+mv $MAVEN_FILE $MAVEN_PATH
 git add $RELEASE_PATH
-git ci $RELEASE_PATH -m "update release"
+git add $MAVEN_PATH
+git ci $RELEASE_PATH $MAVEN_PATH -m "update release"
 git push origin gh-pages
 git checkout master
