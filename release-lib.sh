28a2405c02ab0ab2b3ffe7f0d37b0bcaad88f2c5
vrunoa <vruno.alassia@gmail.com>
2016-01-14 23:28:51 -0800

    update release script

--- release-lib.sh
+++ release-lib.sh
@@ -7,17 +7,14 @@ if [ ! -f $RELEASE_NAME ]; then
   exit 1
   echo "buiild release $RELEASE_NAME not found!"
 fi
-
 git checkout gh-pages
 RELEASE_FOLDER=$"com/urucas/logcatio/${VERSION}/"
 if [ ! -d $RELEASE_FOLDER ]; then
   mkdir $RELEASE_FOLDER
 fi
 RELEASE_PATH=$"${RELEASE_FOLDER}logcatio-${VERSION}.aar"
-echo $RELEASE_PATH
-exit 1
 mv  $RELEASE_NAME $RELEASE_PATH
-exit 1
-git ci com/urucas/logcatio/1.0.1/logcatio-1.0.1.aar -m "update release"
+git add $RELEASE_PATH
+git ci $RELEASE_PATH -m "update release"
 git push origin gh-pages
 git checkout master
