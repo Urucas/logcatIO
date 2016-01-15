3e31656c5bd2d1c855b96c2d30a97316fc3be8db
vrunoa <vruno.alassia@gmail.com>
2016-01-15 01:41:26 -0800

    add an edittext with a textwatcher that logs every change

--- logcatio-app/src/main/java/com/urucas/logcatio_app/MainActivity.java
+++ logcatio-app/src/main/java/com/urucas/logcatio_app/MainActivity.java
@@ -2,10 +2,13 @@ package com.urucas.logcatio_app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by vruno on 1/15/16.
@@ -18,12 +21,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG_NAME, "MainActivity launched");
 
         Button logBtt = (Button) findViewById(R.id.logBtt);
         logBtt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG_NAME, "button pressed");
+                Log.i(TAG_NAME, "Button Pressed");
+            }
+        });
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.i(TAG_NAME, "About to change text: "+ charSequence.toString());
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                Log.i(TAG_NAME, "Changed text: "+ charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
