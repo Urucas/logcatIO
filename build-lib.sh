84984c43455700c3aa93c0d29abf1f50ed40a94c
vrunoa <vruno.alassia@gmail.com>
2016-01-14 23:14:09 -0800

    update build script

--- build-lib.sh
+++ build-lib.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+VERSION=$(./gradlew -q Version)
+echo "Project Version"
+echo $VERSION
+RELEASE_NAME=$"./logcatio-${VERSION}.aar"
+if [ -f $RELEASE_NAME ]; then
+  rm $RELEASE_NAME 
+fi
 rm -rf ./build
-rm logcatio-1.0.1.aar
 ./gradlew aR
-cp ./lib/build/outputs/aar/lib-release.aar ./logcatio-1.0.1.aar
+cp ./lib/build/outputs/aar/lib-release.aar $RELEASE_NAME
